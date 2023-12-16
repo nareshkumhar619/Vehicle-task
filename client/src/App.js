2a3171d046e0346d934bf1fd18eabb8094b4eebc
Naresh Kumhar <122338149+nareshkumhar619@users.noreply.github.com>
2023-12-16 19:31:52 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css'; // Import the CSS file
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css"; // Import the CSS file
 
 function App() {
   const [vehicles, setVehicles] = useState([]);
   const [newVehicle, setNewVehicle] = useState({
-    chassisNumber: '',
-    registrationNumber: '',
+    chassisNumber: "",
+    registrationNumber: "",
     lastLocation: { lat: 0, long: 0 },
-    kind: 'Reefer',
+    kind: "Reefer",
     milesDriven: 0,
     lastInspection: new Date().toISOString(),
   });
@@ -20,10 +20,10 @@ function App() {
 
   const fetchVehicles = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/vehicles');
+      const response = await axios.get("https://vehicle-task.onrender.com/api/vehicles");
       setVehicles(response.data);
     } catch (error) {
-      console.error('Error fetching vehicles:', error);
+      console.error("Error fetching vehicles:", error);
     }
   };
 
@@ -37,18 +37,18 @@ function App() {
 
   const addVehicle = async () => {
     try {
-      await axios.post('http://localhost:5000/vehicles', newVehicle);
+      await axios.post("https://vehicle-task.onrender.com/api/vehicles", newVehicle);
       setNewVehicle({
-        chassisNumber: '',
-        registrationNumber: '',
+        chassisNumber: "",
+        registrationNumber: "",
         lastLocation: { lat: 0, long: 0 },
-        kind: 'Reefer',
+        kind: "Reefer",
         milesDriven: 0,
         lastInspection: new Date().toISOString(),
       });
       fetchVehicles();
     } catch (error) {
-      console.error('Error adding vehicle:', error);
+      console.error("Error adding vehicle:", error);
     }
   };
 
@@ -60,30 +60,30 @@ function App() {
   const updateVehicle = async () => {
     try {
       await axios.put(
-        `http://localhost:5000/vehicles/${editingVehicle._id}`,
+        `https://vehicle-task.onrender.com/api/vehicles/${editingVehicle._id}`,
         newVehicle
       );
       setEditingVehicle(null);
       setNewVehicle({
-        chassisNumber: '',
-        registrationNumber: '',
+        chassisNumber: "",
+        registrationNumber: "",
         lastLocation: { lat: 0, long: 0 },
-        kind: 'Reefer',
+        kind: "Reefer",
         milesDriven: 0,
         lastInspection: new Date().toISOString(),
       });
       fetchVehicles();
     } catch (error) {
-      console.error('Error updating vehicle:', error);
+      console.error("Error updating vehicle:", error);
     }
   };
 
   const deleteVehicle = async (id) => {
     try {
-      await axios.delete(`http://localhost:5000/vehicles/${id}`);
+      await axios.delete(`https://vehicle-task.onrender.com/api/vehicles/${id}`);
       fetchVehicles();
     } catch (error) {
-      console.error('Error deleting vehicle:', error);
+      console.error("Error deleting vehicle:", error);
     }
   };
 
@@ -91,7 +91,7 @@ function App() {
     <div className="app-container">
       <h1>Vehicle Management App</h1>
       <div className="form-container">
-        <h2>{editingVehicle ? 'Edit Vehicle' : 'Add a New Vehicle'}</h2>
+        <h2>{editingVehicle ? "Edit Vehicle" : "Add a New Vehicle"}</h2>
         <label>Chassis Number:</label>
         <input
           type="text"
@@ -121,7 +121,7 @@ function App() {
           onChange={handleInputChange}
         />
         <label>Last Location:</label>
-        
+
         <label>Last Inspection:</label>
         <input
           type="text"
@@ -129,7 +129,7 @@ function App() {
           value={newVehicle.lastInspection}
           onChange={handleInputChange}
         />
-        
+
         {editingVehicle ? (
           <button className="update-button" onClick={updateVehicle}>
             Update Vehicle
@@ -145,12 +145,18 @@ function App() {
         <ul>
           {vehicles.map((vehicle) => (
             <li key={vehicle._id} className="vehicle-item">
-              {vehicle.chassisNumber} - {vehicle.registrationNumber} -{' '}
+              {vehicle.chassisNumber} - {vehicle.registrationNumber} -{" "}
               {vehicle.kind}
-              <button className="edit-button" onClick={() => editVehicle(vehicle)}>
+              <button
+                className="edit-button"
+                onClick={() => editVehicle(vehicle)}
+              >
                 Edit
               </button>
-              <button className="delete-button" onClick={() => deleteVehicle(vehicle._id)}>
+              <button
+                className="delete-button"
+                onClick={() => deleteVehicle(vehicle._id)}
+              >
                 Delete
               </button>
             </li>
